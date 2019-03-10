de8f50eca15ddc18bbffa7f29de3030cbd4a5b18
ycysvler <ycysvler@hotmail.com>
2019-03-10 23:15:18 +0800

    * use pm2

--- test/versions.py
+++ test/versions.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # pip install Flask
+
 # pip intall flask_script
+# python app.py runserver  -p 8000 -d -r --thread : 使用8000端口，开启debug调试模式，自动重启，使用多线程。
+
+# pip install gunicorn
+#  gunicorn -w 4 -b 127.0.0.1:4000 versions:app
+
 from flask import Flask, abort, request,jsonify
 from flask_script import Manager
 
 app = Flask(__name__)
 
-manager = Manager(app)
+#manager = Manager(app)
 
 @app.route('/versions')
 def hello_world():
@@ -24,4 +30,4 @@ def hello_world():
 
 if __name__ == "__main__":
     # 这种是不太推荐的启动方式，我这只是做演示用，官方启动方式参见：http://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
-    manager.run()
+    app.run()
