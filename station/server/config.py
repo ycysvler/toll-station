78ee4d18c50ff4146b3d2c9f18145b95bc7f11cd
ycysvler <ycysvler@hotmail.com>
2019-08-19 00:02:03 +0800

    * 增加管理端的图标，增加监控页面

--- station/server/config.py
+++ station/server/config.py
@@ -1,3 +1,3 @@
-center_base = "http://localhost:4101"
+center_base = "http://172.17.0.3:4101"
 local_root_path = './'
 local_models_path = '/home/parallels/projects/toll-station/src/station/server/static/models/'
